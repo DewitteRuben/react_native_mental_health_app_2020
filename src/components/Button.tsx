381b26857457d656bdd86075b4631c9e149fb1ef
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-06 22:31:40 +0200

    feat: button icon will match active state color

---
+++ src/components/Button.tsx
@@ -0,0 +1,68 @@
+import React, { ReactNode, ReactElement } from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const buttonStyles = StyleSheet.create({
+  normalContainer: {
+    backgroundColor: "#F4F4F4",
+  },
+  highlightedContainer: {
+    backgroundColor: "#7A7977",
+  },
+  iconMargin: {
+    marginLeft: 8,
+  },
+  text: {
+    textTransform: "uppercase",
+    fontWeight: "700",
+    textAlign: "center",
+  },
+  normalText: {
+    color: "#000000",
+  },
+  highlightedText: {
+    color: "#ffffff",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "flex-start",
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    borderRadius: 25,
+  },
+});
+
+interface IButtonProps {
+  text: string;
+  active?: boolean;
+  icon?: React.ReactElement;
+}
+
+export default class Button extends React.Component<IButtonProps, {}> {
+  render() {
+    const { active, icon, text } = this.props;
+
+    const textStyles = [buttonStyles.text, active ? buttonStyles.highlightedText : buttonStyles.normalText];
+
+    const containerStyles = [
+      buttonStyles.container,
+      active ? buttonStyles.highlightedContainer : buttonStyles.normalContainer,
+    ];
+
+    return (
+      <TouchableOpacity style={containerStyles}>
+        {icon && React.cloneElement(icon, { style: textStyles })}
+        <Text style={[textStyles, icon ? buttonStyles.iconMargin : null]}>{text}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
