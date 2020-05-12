5f4e4f66ce803711308b865f653465007c788e67
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-12 15:27:47 +0200

    feat: added padded prop to container to toggle padding

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+interface IContainerProps {
+  padded?: boolean;
+}
 
 const containerStyles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     flex: 1,
   },
+  padding: {
+    paddingHorizontal: 20,
+  },
 });
 
-const Container: React.FC = ({ children }) => <View style={containerStyles.container}>{children}</View>;
+const Container: React.FC<IContainerProps> = ({ children, padded }) => (
+  <View style={[containerStyles.container, padded && containerStyles.padding]}>{children}</View>
+);
 
 export default Container;
