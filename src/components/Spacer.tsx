6d39e0c4b998043de00e678d670dfb73ac3f361a
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-12 21:17:30 +0200

    feat: added vertical prop to spacer component

--- src/components/Spacer.tsx
+++ src/components/Spacer.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
-const getSizeStyle = (size: number) => {
+const getSizeStyle = (size: number, vertical?: boolean) => {
   return {
     container: {
-      height: size,
+      [vertical ? "width" : "height"]: size,
     },
   };
 };
 
-const spacerStyles = StyleSheet.create({
-  ...getSizeStyle(16),
-});
-
 interface ISpacerProps {
   size?: number;
+  vertical?: boolean;
 }
 
 export default class Spacer extends React.Component<ISpacerProps> {
   render() {
-    const { size } = this.props;
-    return <View style={size ? getSizeStyle(size).container : spacerStyles.container} />;
+    const { size, vertical } = this.props;
+    return <View style={size ? getSizeStyle(size, vertical).container : getSizeStyle(16, vertical).container} />;
   }
 }
