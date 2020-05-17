b6111f40a8ec9de86d56f8ede119219dfcfdaea2
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-17 15:00:37 +0200

    feat: ignore stacked virtualized list warning

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import {  renderMainNavigationTabs } from "./src/navigation/navigation";
+import { YellowBox } from "react-native";
+
+YellowBox.ignoreWarnings(["VirtualizedLists should never be nested"]);
 
 export default function App() {
   return <NavigationContainer>{renderMainNavigationTabs()}</NavigationContainer>;
