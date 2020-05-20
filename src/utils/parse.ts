1a8a966e17847eba76296eac252e26ecafe43332
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-20 20:08:05 +0200

    feat: uuid validation

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -8,3 +8,16 @@ export const dateReviver = function (key: any, value: any) {
   }
   return value;
 };
+
+const uuid: { [key: string]: RegExp } = {
+  3: /^[0-9A-F]{8}-[0-9A-F]{4}-3[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
+  4: /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+  5: /^[0-9A-F]{8}-[0-9A-F]{4}-5[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+  all: /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
+};
+
+export function isUUID(str: string) {
+  const version = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "all";
+  const pattern = uuid[version];
+  return pattern && pattern.test(str);
+}
