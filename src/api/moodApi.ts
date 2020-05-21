385389280ae2b69b9ffc5d19294d86e9c4c8451d
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-21 23:01:39 +0200

    feat: added new endpoints to moodapi

--- src/api/moodApi.ts
+++ src/api/moodApi.ts
@@ -1,7 +1,29 @@
 import { request } from ".";
+import { IExperienceItem } from "../components/ExperienceSelector";
+
+export interface IMoodEntry {
+  userId?: string;
+  thoughts?: string;
+  entryId: string;
+  emotions: string[];
+  experiences: IExperienceItem[];
+  sleep: number;
+  mood: string;
+  date: Date;
+}
+
+const authHeader = (token: string) => ({
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
 
 const moodApiFetchAllMoodEntries = async (token: string) => {
-  return request("/moodentry", { headers: { Authorization: `Bearer ${token}` } });
+  return request("/moodentry", authHeader(token));
 };
 
-export { moodApiFetchAllMoodEntries };
+const moodApiFetchCreateMoodEntry = async (token: string, moodEntry: IMoodEntry) => {
+  return request("/moodentry", { method: "POST", body: JSON.stringify(moodEntry), ...authHeader(token) });
+};
+
+export { moodApiFetchAllMoodEntries, moodApiFetchCreateMoodEntry };
