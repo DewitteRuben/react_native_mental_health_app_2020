ea507181d2b0be65160d5d6cf912e8cd581bfc59
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-21 23:26:36 +0200

    feat: added add new entry basic action

--- src/redux/mood/actions/moodActions.ts
+++ src/redux/mood/actions/moodActions.ts
@@ -1,11 +1,19 @@
 import { Action, ActionCreator } from "redux";
+import { IMoodEntry } from "../../../api/moodApi";
 
 const MOOD_ACTION_TYPES = {
   SET_MOOD_ENTRIES: "set_mood_entries",
   FETCH_ALL_MOOD_ENTRIES: "fetch_all_mood_entries",
   FETCH_ALL_MOOD_ENTRIES_SUCCESS: "fetch_all_mood_entries_success",
   FETCH_ALL_MOOD_ENTRIES_ERROR: "fetch_all_mood_entries_error",
+  FETCH_ADD_NEW_MOOD_ENTRIES: "fetch_add_new_mood_entries",
+  FETCH_ADD_NEW_MOOD_ENTRIES_SUCCESS: "fetch_add_new_mood_entries_success",
+  FETCH_ADD_NEW_MOOD_ENTRIES_ERROR: "fetch_add_new_mood_entries_error",
 };
+
+interface IFetchAddNewMoodEntry extends Action {
+  entry: IMoodEntry;
+}
 
 interface IFetchAllMoodEntries extends Action {}
 interface IFetchAllMoodEntriesSuccess extends Action {}
@@ -16,6 +24,11 @@ interface IFetchAllMoodEntriesError extends Action {
 interface ISetMoodEntries extends Action {
   entries: [];
 }
+
+const FetchAddNewMoodEntryAction: ActionCreator<IFetchAddNewMoodEntry> = (entry: IMoodEntry) => ({
+  type: MOOD_ACTION_TYPES.FETCH_ADD_NEW_MOOD_ENTRIES,
+  entry,
+});
 
 const SetMoodEntriesAction: ActionCreator<ISetMoodEntries> = (entries: []) => ({
   type: MOOD_ACTION_TYPES.SET_MOOD_ENTRIES,
@@ -35,10 +48,15 @@ const FetchAllMoodEntriesErrorAction: ActionCreator<IFetchAllMoodEntriesError> =
   error,
 });
 
+export type MoodAction = IFetchAddNewMoodEntry | IFetchAllMoodEntries;
+
 export {
   MOOD_ACTION_TYPES,
   SetMoodEntriesAction,
   FetchAllMoodEntriesAction,
   FetchAllMoodEntriesErrorAction,
   FetchAllMoodEntrieSucesssAction,
+  FetchAddNewMoodEntryAction,
+  IFetchAddNewMoodEntry,
+  IFetchAllMoodEntries,
 };
--- src/redux/mood/reducer/moodReducer.ts
+++ src/redux/mood/reducer/moodReducer.ts
@@ -1,15 +1,21 @@
 import { Reducer, AnyAction } from "redux";
+import { MoodAction, MOOD_ACTION_TYPES, IFetchAddNewMoodEntry } from "../actions/moodActions";
+import { IMoodEntry } from "../../../api/moodApi";
 
 const initialState: IMoodState = {
   entries: [],
 };
 
 export interface IMoodState {
-  entries: [];
+  entries: IMoodEntry[];
 }
 
-const moodReducer: Reducer<IMoodState> = (state = initialState, action: AnyAction) => {
-  switch (action) {
+const moodReducer: Reducer<IMoodState> = (state = initialState, action: MoodAction) => {
+  switch (action.type) {
+    case MOOD_ACTION_TYPES.FETCH_ADD_NEW_MOOD_ENTRIES:
+      const { entries } = state;
+      const { entry } = <IFetchAddNewMoodEntry>action;
+      return { ...state, entries: [...entries, entry] };
     default:
       return state;
   }
