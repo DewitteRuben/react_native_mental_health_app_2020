1fb872c3a93a1d48e1759bca4c23116c5472d34b
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-21 23:27:09 +0200

    feat: added save mood entry functionality to form

--- src/views/AddMoodEntry.tsx
+++ src/views/AddMoodEntry.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import Container from "../components/Container";
 import AddMoodEntryForm, { INewMoodEntry } from "../components/AddMoodEntryForm";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { FetchAddNewMoodEntryAction, IFetchAddNewMoodEntry } from "../redux/mood/actions/moodActions";
+import { IMoodEntry } from "../api/moodApi";
+import { MoodAction } from "../redux/mood/actions/moodActions";
 import { PartialBy } from "../utils/types";
 
-export default class AddMoodEntry extends React.Component {
+interface IAddMoodEntryProps {
+  fetchAddNewMoodEntry: (entry: IMoodEntry) => IFetchAddNewMoodEntry;
+}
+
+class AddMoodEntry extends React.Component<IAddMoodEntryProps> {
+  handleOnSave = (entry: PartialBy<INewMoodEntry, "thoughts">) => {
+    const { fetchAddNewMoodEntry } = this.props;
+    fetchAddNewMoodEntry(entry);
+  };
+
   render() {
     return (
       <Container scroll padded>
-        <AddMoodEntryForm />
+        <AddMoodEntryForm onSave={this.handleOnSave} />
       </Container>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch: Dispatch<MoodAction>) => ({
+  fetchAddNewMoodEntry: (entry: IMoodEntry) => dispatch(FetchAddNewMoodEntryAction(entry)),
+});
+
+export default connect(null, mapDispatchToProps)(AddMoodEntry);
