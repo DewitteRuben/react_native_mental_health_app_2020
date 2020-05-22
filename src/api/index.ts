67f5b426d272b7d6eee78b058be2556381c9d2db
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-23 00:49:56 +0200

    fix: properly merge default option with optional options

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,22 @@
 import { BASE_API_LOCAL } from "../../env.json";
 import ApiError from "../errors/ApiError";
+import _ from "lodash";
 
 const request = async (endpoint: string, requestInit?: RequestInit, parseAs: "json" | "string" = "json") => {
   if (!endpoint.includes("/")) {
     throw new Error("The endpoint must be prepended with /");
   }
-  const resp = await fetch(`${BASE_API_LOCAL}${endpoint}`, {
-    headers: {
-      "Content-Type": "application/json",
+
+  const options = _.merge(
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
     },
-    ...requestInit,
-  });
+    requestInit
+  );
+
+  const resp = await fetch(`${BASE_API_LOCAL}${endpoint}`, options);
   const data = await (parseAs === "json" ? resp.json() : resp.text());
   if (resp.status !== 200) {
     throw new ApiError(data.error.message, resp.status);
