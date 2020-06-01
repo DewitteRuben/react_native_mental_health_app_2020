36130b292ee70bed3a7883f6fd525504cd5b9fab
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-06-01 19:22:46 +0200

    fix: sync task no longer stops prematurely

--- src/redux/mood/sagas/moodSagas.ts
+++ src/redux/mood/sagas/moodSagas.ts
@@ -37,56 +37,54 @@ function* cancelSyncTask(syncTask: Task) {
 }
 
 function* syncProcess() {
-  while (true) {
-    const netInfoState: NetInfoState = yield call(NetInfo.fetch);
-    const localQueue: string[] = yield select(selectEntryQueue);
+  const netInfoState: NetInfoState = yield call(NetInfo.fetch);
+  const localQueue: string[] = yield select(selectEntryQueue);
 
-    if (!netInfoState.isInternetReachable) {
-      continue;
-    }
+  if (!netInfoState.isInternetReachable) {
+    return;
+  }
 
-    const token = yield call(getToken);
-    if (!token) {
-      continue;
-    }
+  const token = yield call(getToken);
+  if (!token) {
+    return;
+  }
 
-    const localEntryIDs: string[] = yield select(entryIdsSelector);
-    const remoteEntryIDs = yield call(moodApiFetchAllMoodEntryIDs, token);
-    const entryIdsNotLocalYet = _.difference(remoteEntryIDs, localEntryIDs);
+  const localEntryIDs: string[] = yield select(entryIdsSelector);
+  const remoteEntryIDs = yield call(moodApiFetchAllMoodEntryIDs, token);
+  const entryIdsNotLocalYet = _.difference(remoteEntryIDs, localEntryIDs);
 
-    if (!entryIdsNotLocalYet.length) {
-      continue;
-    }
-
+  // Fetch missing entries from remote db
+  if (entryIdsNotLocalYet.length) {
     const entriesToFetch = yield all(
       entryIdsNotLocalYet.map((entryId) => call(moodApiFetchMoodEntryByEntryId, token, entryId))
     );
 
     const localEntries = yield select(selectEntries);
+
     yield put(SetMoodEntriesAction([...localEntries, ...entriesToFetch[0]]));
-
-    if (!localQueue.length) {
-      continue;
-    }
-
-    const entryIdQueueForLive = _.difference(localQueue, remoteEntryIDs);
-
-    // Entries to push to remote db
-    const entryQueueForLive = yield select(idsToEntries(entryIdQueueForLive));
-
-    // Push all entries in parallel
-    const itemsPushedLive = yield all(
-      entryQueueForLive.map((entry: IMoodEntry) => call(moodApiFetchCreateMoodEntry, token, entry))
-    );
-
-    // Clear queue after pushing live
-    yield put(ClearSyncQueueAction());
   }
+
+  if (!localQueue.length) {
+    return;
+  }
+
+  const entryIdQueueForLive = _.difference(localQueue, remoteEntryIDs);
+
+  // Entries to push to remote db
+  const entryQueueForLive = yield select(idsToEntries(entryIdQueueForLive));
+
+  // Push all entries in parallel
+  const itemsPushedLive = yield all(
+    entryQueueForLive.map((entry: IMoodEntry) => call(moodApiFetchCreateMoodEntry, token, entry))
+  );
+
+  // Clear queue after pushing live
+  yield put(ClearSyncQueueAction());
 }
 
 function* sync() {
   while (true) {
-    syncProcess();
+    yield syncProcess();
 
     // Wait before next sync
     yield delay(5000);
