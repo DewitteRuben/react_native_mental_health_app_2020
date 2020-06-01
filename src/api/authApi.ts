1aaee3ecbd0d2878a56700bf3c17783135b1b502
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-06-01 13:51:39 +0200

    feat: update endpoint to match server

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -2,7 +2,7 @@ import { request } from ".";
 
 const authApiFetchAuth = async (userId: string) => {
   const body = JSON.stringify({ userId });
-  return request("/auth", { method: "POST", body });
+  return request("/auth/user", { method: "POST", body });
 };
 
 export { authApiFetchAuth };
